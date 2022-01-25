c907f6e6a4bf03c20b25e3baf6da024ef35f2be7
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-01-25 16:50:15 -0300

    learning about modules

--- first-project/src/app/app.module.ts
+++ first-project/src/app/app.module.ts
@@ -3,8 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
 import { MyFirstComponent } from './my-first/my-first.component';
 import { MyFirst2Component } from './my-first2/my-first2.component';
+
+
+/*
+  Declarations: é a parte onde são identificados todos os componentes, diretivas e pipes utilizadas pelo modulo.
+
+  Imports: são declarados os modulos que você quer deixar disponível para os componentese deste modulo em questão.
+
+  Providers: nesta parte são declarados todos os serviços que ficaram disponível para o uso dos modulos.
+
+  Bootstrap: Serve para instânciar o primeiro componente a ser renderizado na tela, este componente vai servir de container para os demais componentes.
+*/
 
 @NgModule({
   declarations: [
@@ -14,7 +26,8 @@ import { MyFirst2Component } from './my-first2/my-first2.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
