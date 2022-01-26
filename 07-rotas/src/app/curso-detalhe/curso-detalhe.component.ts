173416cb78f5bf1ceab67a9c37fb3a869723cf46
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-01-26 12:18:43 -0300

    feat: adding the logic to show the course name and page not found

--- 07-rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ 07-rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription, UnsubscriptionError } from 'rxjs';
+import { CursosService } from '../cursos/cursos.service';
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -10,16 +11,27 @@ import { Subscription, UnsubscriptionError } from 'rxjs';
 export class CursoDetalheComponent implements OnInit, OnDestroy {
   id: number = 0;
   inscricao !: Subscription;
+  curso?: any;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private cursosService: CursosService,
+    private router: Router,
+  ){
     // this.id = this.route.snapshot.params['id'];
     // console.log('Now: ', this.id)
   }
 
   ngOnInit(): void {
     this.inscricao = this.route.params.subscribe(params => {
-      this.id = params['id']
+      this.id = params['id'];
+
+      this.curso = this.cursosService.getCurso(this.id)
+
+      if(!this.curso)
+      this.router.navigate(['/not-found'])
     })
+
   }
 
   ngOnDestroy (): void {
