5d2a93b5262ca0b6716cca317a4eb1f28c9ead16
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-01-26 12:17:32 -0300

    feat: adding the methods to get courses

--- 07-rotas/src/app/cursos/cursos.service.ts
+++ 07-rotas/src/app/cursos/cursos.service.ts
@@ -6,4 +6,21 @@ import { Injectable } from '@angular/core';
 export class CursosService {
 
   constructor() { }
+
+  getCursos() {
+    return [
+      { id: 1, name: 'Angular 2' },
+      { id: 2, name: 'Javascrip puro' },
+      { id: 3, name: 'VueJs 3' },
+    ];
+  }
+
+  getCurso(id: number) {
+    let cursos = this.getCursos()
+    for(let curso of cursos) {
+      if(curso.id == id)
+        return curso
+    }
+    return null
+  }
 }
