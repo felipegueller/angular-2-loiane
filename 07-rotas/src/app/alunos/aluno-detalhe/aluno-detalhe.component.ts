9197d0f7454194137b41bd7306959f02956d1fe7
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-01-27 13:43:24 -0300

    feat: showing the alunos details

--- 07-rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
+++ 07-rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Aluno } from '../aluno.model';
+import { AlunosService } from '../alunos.service';
 
 @Component({
   selector: 'app-aluno-detalhe',
   templateUrl: './aluno-detalhe.component.html',
-  styleUrls: ['./aluno-detalhe.component.css']
+  styleUrls: ['./aluno-detalhe.component.css'],
 })
-export class AlunoDetalheComponent implements OnInit {
+export class AlunoDetalheComponent implements OnInit, OnDestroy {
+  subscriptions: Array<Subscription> = [];
+  id!: number;
+  aluno!: Aluno;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private alunoService: AlunosService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.subscriptions.push(
+      this.route.params.subscribe((params) => {
+        this.id = params['id'];
+        this.aluno = this.alunoService.getAluno(this.id);
+      })
+    );
   }
 
+  editarAluno(): void {
+    this.router.navigate(['/alunos', this.id, 'editar']);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => {
+      subscription.unsubscribe;
+    });
+  }
 }
