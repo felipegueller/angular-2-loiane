30f5fd4d2367e117265bc7c51c28067379b7e8ce
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-01-27 11:05:23 -0300

    feat: creating cursos.module

---
+++ 07-rotas/src/app/cursos/cursos.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursosComponent } from './cursos.component';
+import { CursosService } from './cursos.service';
+
+NgModule({
+  imports: [CommonModule],
+  exports: [],
+  declarations: [
+    CursosComponent,
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent,
+  ],
+  providers: [CursosService],
+});
+export class CursosModule {}
