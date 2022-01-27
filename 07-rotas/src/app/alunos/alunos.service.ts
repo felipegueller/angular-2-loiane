64e3065cbbf38b443d4d351d77319de9e81b5206
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-01-27 13:40:52 -0300

    feat: implementig the methods of alunos.service

--- 07-rotas/src/app/alunos/alunos.service.ts
+++ 07-rotas/src/app/alunos/alunos.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Aluno } from './aluno.model';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class AlunosService {
+  private alunos: Array<Aluno> = [
+    { id: 1, nome: 'Aluno 01', email: 'aluno01@mail.com' },
+    { id: 2, nome: 'Aluno 02', email: 'aluno02@mail.com' },
+    { id: 3, nome: 'Aluno 03', email: 'aluno03@mail.com' },
+    { id: 4, nome: 'Aluno 04', email: 'aluno04@mail.com' },
+    { id: 5, nome: 'Aluno 05', email: 'aluno05@mail.com' },
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  getAlunos(): Array<Aluno> {
+    return this.alunos;
+  }
+
+  getAluno(id: number): Aluno {
+    for (let aluno of this.getAlunos()) {
+      if (id == aluno.id) return aluno;
+    }
+    return { id: null!, nome: '', email: '' };
+  }
 }
