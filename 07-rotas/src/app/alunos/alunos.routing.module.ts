5ea4a2848e8d3966da93262ae65ab3bb93170e19
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-01-27 12:14:24 -0300

    feat: setting the children routes logic

--- 07-rotas/src/app/alunos/alunos.routing.module.ts
+++ 07-rotas/src/app/alunos/alunos.routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
+import { AlunosFormComponent } from './alunos-form/alunos-form.component';
+import { AlunosComponent } from './alunos.component';
 
-const alunosRoutes: Routes = [];
+const alunosRoutes: Routes = [
+  {
+    path: 'alunos',
+    component: AlunosComponent,
+    children: [
+      { path: 'novo', component: AlunosFormComponent },
+      { path: ':id', component: AlunoDetalheComponent },
+      { path: ':id/editar', component: AlunosFormComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(alunosRoutes)],
