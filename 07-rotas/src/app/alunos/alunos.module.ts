86392fd4436c250a4da6c59117f4573dcfc7a1fb
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-01-27 12:15:16 -0300

    feat: creating the alunos.service

--- 07-rotas/src/app/alunos/alunos.module.ts
+++ 07-rotas/src/app/alunos/alunos.module.ts
@@ -5,11 +5,12 @@ import { AlunosComponent } from './alunos.component';
 import { AlunosFormComponent } from './alunos-form/alunos-form.component';
 import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
 import { AlunosRoutingModule } from './alunos.routing.module';
+import { AlunosService } from './alunos.service';
 
 @NgModule({
   imports: [CommonModule, AlunosRoutingModule],
   exports: [],
   declarations: [AlunosComponent, AlunosFormComponent, AlunoDetalheComponent],
-  providers: [],
+  providers: [AlunosService],
 })
 export class AlunosModule {}
