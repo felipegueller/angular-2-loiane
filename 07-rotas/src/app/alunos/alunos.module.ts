d76adc1938fd5b3759e77984208621f359d46f3b
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-01-27 11:53:36 -0300

    feat: creating the aluno-detalhe.component

--- 07-rotas/src/app/alunos/alunos.module.ts
+++ 07-rotas/src/app/alunos/alunos.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AlunosComponent } from './alunos.component';
 import { AlunosFormComponent } from './alunos-form/alunos-form.component';
+import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
 
 @NgModule({
   imports: [CommonModule],
   exports: [],
-  declarations: [AlunosComponent, AlunosFormComponent],
+  declarations: [AlunosComponent, AlunosFormComponent, AlunoDetalheComponent],
   providers: [],
 })
 export class AlunosModule {}
