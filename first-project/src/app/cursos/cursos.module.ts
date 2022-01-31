82051c49d9c66c2fa30a2d0d03851bd4185460cc
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-01-31 09:03:48 -0300

    feat: adding the cursos.service in providers property

--- first-project/src/app/cursos/cursos.module.ts
+++ first-project/src/app/cursos/cursos.module.ts
@@ -3,19 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { CursosComponent } from './cursos.component';
 import { CursoDetalhesComponent } from './curso-detalhes/curso-detalhes.component';
-
-
+import { CursosService } from './cursos.service';
 
 @NgModule({
-  declarations: [
-    CursosComponent,
-    CursoDetalhesComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    CursosComponent,
-  ]
+  declarations: [CursosComponent, CursoDetalhesComponent],
+  imports: [CommonModule],
+  exports: [CursosComponent],
+  providers: [CursosService],
 })
-export class CursosModule { }
+export class CursosModule {}
