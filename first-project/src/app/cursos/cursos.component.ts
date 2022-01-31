53b4ff74e4b460a5db4c00695f79e84989442e55
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-01-31 09:04:55 -0300

    feat: using the dependence injection to get the courses name

--- first-project/src/app/cursos/cursos.component.ts
+++ first-project/src/app/cursos/cursos.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+  styleUrls: ['./cursos.component.css'],
 })
 export class CursosComponent implements OnInit {
-
-  constructor() { }
+  cursos!: Array<string>;
+  constructor(private cursosService: CursosService) {}
 
   ngOnInit(): void {
+    this.cursos = this.cursosService.getCursos();
   }
-
 }
--- first-project/src/app/cursos/cursos.service.ts
+++ first-project/src/app/cursos/cursos.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 
+/*
+   A injeção de dependência providencia uma instância da classe service, sem a necessidade de fazer também as intâncias que a classe service utiliza. Sendo assim, você tem a possibiliade de usar todos os métodos fornecidos utilizando os métodos de terceiros.
+*/
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CursosService {
+  constructor() {}
 
-  constructor() { }
+  getCursos() {
+    return ['Java', 'Javascript', 'C#', 'Ruby', 'Python'];
+  }
 }
