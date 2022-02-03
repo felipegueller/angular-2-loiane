c68d9de208d4f8ece0390bab265437324d38008f
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-02-03 09:27:10 -0300

    feat: adding the logic for athenticated access1

--- 07-rotas/src/app/guards/auth.guard.ts
+++ 07-rotas/src/app/guards/auth.guard.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AuthService } from '../login/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGuardService {
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor() { }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    if (this.authService.usuarioEstaAutenticado()) return true;
+    this.router.navigate(['/login']);
+    return false;
+  }
 }
