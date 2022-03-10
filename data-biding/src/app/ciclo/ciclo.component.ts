04204d57503b98c98d2ea51880e4d539f5cddb8e
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-03-10 12:29:57 -0300

    feat: learning more about Angular's lifecycles

--- data-biding/src/app/ciclo/ciclo.component.ts
+++ data-biding/src/app/ciclo/ciclo.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-ciclo',
   templateUrl: './ciclo.component.html',
-  styleUrls: ['./ciclo.component.css']
+  styleUrls: ['./ciclo.component.css'],
 })
-export class CicloComponent implements OnInit {
+export class CicloComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewChecked,
+    AfterViewInit,
+    OnDestroy
+{
+  @Input() valorInicial: number = 10;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('Constructor');
   }
 
+  ngOnInit(): void {
+    console.log('OnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const { valorInicial } = changes;
+    console.log('OnChanges', valorInicial.currentValue);
+  }
+
+  ngDoCheck(): void {
+    console.log('DoCheck');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('AfterContentChecked');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('AfterContentInit');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('AfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('AfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('OnDestroy');
+  }
 }
