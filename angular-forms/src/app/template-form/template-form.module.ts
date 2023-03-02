40df7da94f34bd445635c572b2309f932260b456
Felipe Gueller <felipe.gueller@gmail.com>
2023-03-02 16:04:46 -0300

    :sparkles: feat: creating the new inputs and using form-debug selector

--- angular-forms/src/app/template-form/template-form.module.ts
+++ angular-forms/src/app/template-form/template-form.module.ts
@@ -1,3 +1,4 @@
+import { FormDebugComponent } from './../form-debug/form-debug.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -5,7 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { TemplateFormComponent } from './template-form.component';
 
 @NgModule({
-  declarations: [TemplateFormComponent],
+  declarations: [TemplateFormComponent, FormDebugComponent],
   imports: [CommonModule, FormsModule],
   exports: [],
   providers: []
