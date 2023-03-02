26a010cd9bea1550f6920b09177c0c458c9a3525
Felipe Gueller <felipe.gueller@gmail.com>
2023-03-02 07:52:44 -0300

    :sparkles: feat: importin the FormsModule

--- angular-forms/src/app/app.module.ts
+++ angular-forms/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { DataFormComponent } from './data-form/data-form.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
