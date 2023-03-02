4570e2ad524540b8fc54bc38871cf30c9724e52c
Felipe Gueller <felipe.gueller@gmail.com>
2023-03-02 16:59:17 -0300

    :tada: feat: creating the field-error-control component

---
+++ angular-forms/src/app/field-error-control/field-error-control.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-field-error-control',
+  templateUrl: './field-error-control.component.html',
+  styleUrls: ['./field-error-control.component.css']
+})
+export class FieldErrorControlComponent {
+
+}
--- angular-forms/src/app/template-form/template-form.module.ts
+++ angular-forms/src/app/template-form/template-form.module.ts
@@ -1,3 +1,4 @@
+import { FieldErrorControlComponent } from './../field-error-control/field-error-control.component';
 import { FormDebugComponent } from './../form-debug/form-debug.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -6,9 +7,13 @@ import { FormsModule } from '@angular/forms';
 import { TemplateFormComponent } from './template-form.component';
 
 @NgModule({
-  declarations: [TemplateFormComponent, FormDebugComponent],
+  declarations: [
+    TemplateFormComponent,
+    FormDebugComponent,
+    FieldErrorControlComponent,
+  ],
   imports: [CommonModule, FormsModule],
   exports: [],
-  providers: []
+  providers: [],
 })
 export class TemplateFormModule {}
