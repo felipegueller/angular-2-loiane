5520b04e9d391e9d57aad2129e53ee3919839c78
Felipe Gueller <felipe.gueller@gmail.com>
2023-03-02 07:53:40 -0300

    :sparkles: feat: creating the ngSubmit method

--- angular-forms/src/app/template-form/template-form.component.ts
+++ angular-forms/src/app/template-form/template-form.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-template-form',
   templateUrl: './template-form.component.html',
-  styleUrls: ['./template-form.component.css']
+  styleUrls: ['./template-form.component.css'],
 })
-export class TemplateFormComponent {
+export class TemplateFormComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  onSubmit(form: NgForm): void {
+    console.log(form)
+  }
 }
