b44f51c0ea357d13e37c4eb0f49d295da03e4895
Felipe Gueller <felipe.gueller@gmail.com>
2023-03-03 16:27:45 -0300

    :tada: feat: creating the data form module

--- angular-forms/src/app/data-form/data-form.component.ts
+++ angular-forms/src/app/data-form/data-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class DataFormComponent {
 
+  private form!: FormGroup;
 }
