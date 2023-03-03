125abc8099b659063c87d1041f585b2c81bae32a
Felipe Gueller <felipe.gueller@gmail.com>
2023-03-03 17:18:55 -0300

    :sparkles: feat: adding some fields on FormGroup

--- angular-forms/src/app/data-form/data-form.component.ts
+++ angular-forms/src/app/data-form/data-form.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
   templateUrl: './data-form.component.html',
   styleUrls: ['./data-form.component.css']
 })
-export class DataFormComponent {
+export class DataFormComponent implements OnInit {
 
   private form!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: [null],
+      email: [null]
+    })
+  }
 }
