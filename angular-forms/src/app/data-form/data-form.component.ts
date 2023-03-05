2bbfcace96fb00bf73a75a3028b4e0c71b968160
Felipe Gueller <felipe.gueller@gmail.com>
2023-03-05 16:34:48 -0300

    :tada: feat: reseting the form fields

--- angular-forms/src/app/data-form/data-form.component.ts
+++ angular-forms/src/app/data-form/data-form.component.ts
@@ -1,21 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
+import { TemplateFormService } from './../template-form/template-form.service';
+
 @Component({
   selector: 'app-data-form',
   templateUrl: './data-form.component.html',
-  styleUrls: ['./data-form.component.css']
+  styleUrls: ['./data-form.component.css'],
 })
 export class DataFormComponent implements OnInit {
-
   reactiveForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private templateFormService: TemplateFormService
+  ) {}
 
   ngOnInit(): void {
     this.reactiveForm = this.formBuilder.group({
       name: [null],
-      email: [null]
-    })
+      email: [null],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.reactiveForm.value);
+    // this.reactiveForm.reset()
   }
 }
